1e17da87939e67ff1ecbc55728a6fb65921d6863
Robot Lions <chadmusick@gmail.com>
2023-06-01 18:36:27 -0400

    adds ability scores and basic score module

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
@@ -9,7 +9,6 @@ function App() {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [dataArray, setDataArray] = useState(null);
-  const [charName, setCharName] = useState('');
 
   
 
@@ -59,13 +58,40 @@ function BuildName(){
  return assembledName
 }
 
+function ScoreModule(props){
+return dataArray[props.dataArrayIndex]
+}
+
 
   return (
     <div className="container">
       <input id="fileSelect" type="file" onChange={(e)=>setSelectedFile(e.target.files[0])}/>
      <button className="btn btn-primary" onClick={()=>loadFile()}>Load Save File</button>
-     <p>Character Name: {dataArray ? <BuildName /> : null}</p>
-     
+     <p>Character Name: {dataArray ? <BuildName /> : null} </p>
+     <p>Strength: {dataArray ? <ScoreModule dataArrayIndex={16} /> : null} ({dataArray ? <ScoreModule dataArrayIndex={28} /> : null})</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={17} /> : null}</p> */}
+     <p>Intelligence: {dataArray ? <ScoreModule dataArrayIndex={18} /> : null}</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={19} /> : null}</p> */}
+     <p>Wisdom: {dataArray ? <ScoreModule dataArrayIndex={20} /> : null}</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={21} /> : null}</p> */}
+     <p>Dexterity: {dataArray ? <ScoreModule dataArrayIndex={22} /> : null}</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={23} /> : null}</p> */}
+     <p>Constitution: {dataArray ? <ScoreModule dataArrayIndex={24} /> : null}</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={25} /> : null}</p> */}
+     <p>Charisma: {dataArray ? <ScoreModule dataArrayIndex={26} /> : null}</p>
+     {/* <p>Score: {dataArray ? <ScoreModule dataArrayIndex={27} /> : null}</p> */}
+     <p></p>
+
+
+
+
+
+
+
+
+
+
+
      <p>Level: {dataArray ? dataArray[275] : null}</p>
      <p>Experience: {dataArray ? parseInt(((0+dataArray[303].toString(16)).slice(-2)+(0+dataArray[302].toString(16)).slice(-2)+(0+dataArray[301].toString(16)).slice(-2)+(0+dataArray[300].toString(16)).slice(-2)), 16) : null }</p>
     <button className="btn btn-primary" onClick={()=>exportUserInfo()}>Test download</button>
