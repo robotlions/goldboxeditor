65840a5861272057f590635ecac93df0f2382dc3
Robot Lions <chadmusick@gmail.com>
2023-06-07 14:46:34 -0400

    item window now mapping over binary file to extract item names - still can't edit values or effects

--- src/Components/ItemEdit.js
+++ src/Components/ItemEdit.js
@@ -1,85 +1,73 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function ItemEdit(){
+export default function ItemEdit() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [dataArray, setDataArray] = useState(null);
 
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [dataArray, setDataArray] = useState(null);
+  function ItemListModule() {
 
-    function NameModule() {
-        let tempArray = dataArray;
-    
-        let defaultName = assembleName();
-    
-        const [editing, setEditing] = useState(false);
-        const [inputText, setInputText] = useState(defaultName);
-        const editDisplay = (
-          <>
-            <input
-              value={inputText}
-              maxLength={15}
-              onChange={(e) => setInputText(e.target.value)}
-              type="text"
-            />
-            <button
-              onClick={() => {
-                setEditing(!editing);
-                saveName();
-              }}
-            >
-              Done
-            </button>
-          </>
-        );
-    
-        const defaultDisplay = (
-          <>
-            {defaultName} <button onClick={() => setEditing(!editing)}>Edit</button>
-          </>
-        );
-        function assembleName() {
-          let assembledName = "";
-          for (let i = 1; i <= dataArray[0]; i++) {
-            assembledName = assembledName + String.fromCharCode(dataArray[i]);
-          }
-          return assembledName;
-        }
-    
-        function saveName() {
-          tempArray[0] = inputText.length;
-          for (let i = 0; i <= inputText.length; i++) {
-            tempArray[i + 1] = inputText.toUpperCase().charCodeAt(i);
-          }
-          setDataArray(tempArray[113]);
-        }
-    
-        return editing === false ? defaultDisplay : editDisplay;
+
+    const [itemListArray, setItemListArray] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+      if(loading===true){
+        assembleList();
+        setLoading(false);
       }
-
+    },[setItemListArray, itemListArray]);
 
     
+    const defaultDisplay =
+      itemListArray.map((item, index)=><p key={index}>{item}</p>)
+          ;
 
-    function loadFile(file) {
-        if (document.querySelector("#fileSelect").value === "") {
-          alert("No file selected");
-          return;
+    function assembleList() {
+      let nameArray = [];
+      for (let i = 0; i < Array.from(dataArray).length; i++) {
+        if (i === 0 || i % 67 === 0) {
+          nameArray.push(assembleName(i));
         }
-    
-        var reader = new FileReader();
-        reader.onload = function (e) {
-          let data = e.target.result;
-          let dataArray = new Uint8Array(data);
-          setDataArray(dataArray);
-          console.log(dataArray);
-        };
-        reader.onerror = function (e) {
-          console.log("Error : " + e.type);
-        };
-        reader.readAsArrayBuffer(file);
       }
-    
+      setItemListArray(nameArray);
+    }
 
-    return(
-        <div><input
+    function assembleName(index) {
+      let assembledName = "";
+      for (let i = 1; i <= dataArray[index]; i++) {
+        assembledName =
+          assembledName + String.fromCharCode(dataArray[i + index]);
+      }
+      return assembledName;
+    }
+
+
+
+    return defaultDisplay;
+  }
+
+  function loadFile(file) {
+    if (document.querySelector("#fileSelect").value === "") {
+      alert("No file selected");
+      return;
+    }
+
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      let data = e.target.result;
+      let dataArray = new Uint8Array(data);
+      setDataArray(dataArray);
+      console.log(dataArray);
+    };
+    reader.onerror = function (e) {
+      console.log("Error : " + e.type);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  return (
+    <div>
+      <input
         id="fileSelect"
         type="file"
         accept=".stf"
@@ -88,7 +76,7 @@ export default function ItemEdit(){
           loadFile(e.target.files[0]);
         }}
       />
-      {dataArray ? <NameModule /> : null}
-      </div>
-    )
+      {dataArray ? <ItemListModule /> : null}
+    </div>
+  );
 }
