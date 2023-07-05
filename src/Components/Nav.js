e35f909a391744bd1f27194261fd481779f7370f
Chad M <chadmusick@gmail.com>
2023-07-04 23:14:14 -0400

    adds bootstrapped tab navigation

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,37 +1,43 @@
-import {React} from "react";
-import { Navbar, Container} from "react-bootstrap";
+import { React } from "react";
+import { Navbar, Container } from "react-bootstrap";
 import { Nav as MyNav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Nav() {
-
-
   return (
     <Navbar bg="light" variant="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/home/" id="navbrandTitle">
-          Curse of the Secret Pools!
-        </Navbar.Brand>
+        <LinkContainer to="/home/">
+          <Navbar.Brand id="navbrandTitle">
+            Curse of the Secret Pools!
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle className="ms-auto" aria-controls="basic-navbar-nav" />
         <Navbar.Collapse
           className="justify-content-evenly"
           id="basic-navbar-nav"
         >
-          <MyNav onSelect={(key)=>console.log(key)}className="me-auto">
-            <MyNav.Item>
-            <MyNav.Link eventKey="home" href="/home/">Home</MyNav.Link>
-            </MyNav.Item>
-            <MyNav.Item>
-            <MyNav.Link eventKey="poolrad" href="/poolrad/">Pool of Radiance</MyNav.Link>
-            </MyNav.Item>
-            <MyNav.Link href="/azure/">Curse of the Azure Bonds</MyNav.Link>
-
-            <MyNav.Link href="/silverblades/">
-              Secret of the Silver Blades
-            </MyNav.Link>
-            <MyNav.Link href="/pod/">Pools of Darkness</MyNav.Link>
-            <MyNav.Link style={{ fontStyle: "italic" }} href="/binarytool/">
-              Binary File Tool
-            </MyNav.Link>
+          <MyNav className="me-auto" variant="tabs">
+            <LinkContainer to="/home/">
+              <MyNav.Link>Home</MyNav.Link>
+            </LinkContainer>
+            <LinkContainer to="/poolrad/">
+              <MyNav.Link>Pool of Radiance</MyNav.Link>
+            </LinkContainer>
+            <LinkContainer to="/azure">
+              <MyNav.Link>Curse of the Azure Bonds</MyNav.Link>
+            </LinkContainer>
+            <LinkContainer to="/silverblades/">
+              <MyNav.Link>Secret of the Silver Blades</MyNav.Link>
+            </LinkContainer>
+            <LinkContainer to="/pod/">
+              <MyNav.Link>Pools of Darkness</MyNav.Link>
+            </LinkContainer>
+            <LinkContainer to="/binarytool/">
+              <MyNav.Link style={{ fontStyle: "italic" }}>
+                Binary File Tool
+              </MyNav.Link>
+            </LinkContainer>
           </MyNav>
         </Navbar.Collapse>
       </Container>
